2400d38b430c9916158bed0c4fc9c125385882f9
tozen123 <lanzuela.christian@gmail.com>
2023-10-14 13:34:49 +0800

    initial commit

---
+++ LGU-SV Asset Management Sytem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LGU_SV_Asset_Management_Sytem
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
