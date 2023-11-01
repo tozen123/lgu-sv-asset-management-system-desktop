2ecf00ed2b5d6abaf1fe52aae046dff8ec88ac1d
tozen123 <lanzuela.christian@gmail.com>
2023-11-01 22:52:09 +0800

    v0.34b

---
+++ LGU-SV Asset Management Sytem/DialogBox.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace LGU_SV_Asset_Management_Sytem
+{
+    partial class DialogBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.buttonOkay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(13, 13);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(35, 13);
+            this.labelMessage.TabIndex = 0;
+            this.labelMessage.Text = "label1";
+            // 
+            // buttonOkay
+            // 
+            this.buttonOkay.Location = new System.Drawing.Point(158, 155);
+            this.buttonOkay.Name = "buttonOkay";
+            this.buttonOkay.Size = new System.Drawing.Size(188, 56);
+            this.buttonOkay.TabIndex = 1;
+            this.buttonOkay.Text = "button1";
+            this.buttonOkay.UseVisualStyleBackColor = true;
+            // 
+            // DialogBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 223);
+            this.Controls.Add(this.buttonOkay);
+            this.Controls.Add(this.labelMessage);
+            this.Name = "DialogBox";
+            this.Text = "DialogBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.Button buttonOkay;
+    }
+}
